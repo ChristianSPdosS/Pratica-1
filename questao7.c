884b5964e4a496472152fdc4c865ef527bbfa3ea
Christian Soares <89141507+ChristianSPdosS@users.noreply.github.com>
2021-09-30 12:01:24 +0000

    concluido

--- questao7.c
+++ questao7.c
@@ -5,12 +5,14 @@ int main() {
 
   float angulo= 30.0f;
   float distancia= 100.0f;
+  float PI= 3.1416f;
 
-  float altura= (distancia * sin(angulo));
+  float angulo_em_graus= (angulo * PI) / 180;
+
+  float altura= (distancia * sin(angulo_em_graus));
 
   printf("%f\n" , altura);
 
  return 0;
 }
 
-//vezes pi / por 180
--- questaodez.c
+++ questaodez.c
@@ -6,10 +6,16 @@ int main() {
 
 int numero= 5673;
 
-int unidade= numero;
-int dezena= numero;
-int centena= numero;
-int milhar= numero;
+int unidade= numero % 10;
+int numero1= (numero - unidade) / 10;
+
+int dezena= numero1 % 10;
+int numero2= (numero1 - dezena) / 10;
+
+int centena= numero2 % 10;
+int numero3= (numero2 - centena) / 10;
+
+int milhar= numero3;
 
 printf("%d%s%d%s%d%s%d%s\n" , unidade , " unidades, " , dezena , " dezenas, " , centena , " centenas e " , milhar , " milhares" );
 
@@ -17,11 +23,3 @@ printf("%d%s%d%s%d%s%d%s\n" , unidade , " unidades, " , dezena , " dezenas, " , 
 }
 
 
- //num = input;
-    //uni = num % 10;
-   // num = (num - uni)/10;
-   // dez = num % 10;
-   // num = (num - dez)/10;
-    //cent = num % 10;
-   // num = (num - cent)/10;
-   // mil = num % 10;
